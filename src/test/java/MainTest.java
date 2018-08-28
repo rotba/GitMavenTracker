d11147640a4ec4ccec5780fe40b71c60eaad7f26
rotba <rotba@post.bgu.ac.il>
2018-08-28 12:15:17 +0300

    t1 - s, t2 - f

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    private Main main;
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    @Test
+    void Test_1() {
     }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-    @org.junit.jupiter.api.Test
-    void test_1() {
-    }
-    @org.junit.jupiter.api.Test
-    void test_2() {
+    @Test
+    void Test_2() {
+        fail();
     }
 }
