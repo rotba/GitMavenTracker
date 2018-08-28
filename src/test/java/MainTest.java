d38523aa6dfd80307f5018693c72fe3469955488
rotba <rotba@post.bgu.ac.il>
2018-08-28 08:57:30 +0300

    initialized project

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    private Main main;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        main.main(null);
+    }
+}
