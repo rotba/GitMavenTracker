0f67378abb7fb643b2bed73485985d3515fd7277
rotba <rotba@post.bgu.ac.il>
2018-10-09 17:30:43 +0300

    delta_3_Test() will fail when patched

--- sub_mod_1/src/main/java/p_1/Amit.java
+++ sub_mod_1/src/main/java/p_1/Amit.java
@@ -4,7 +4,7 @@ public class Amit {
     public int hoo(){return 5;}
     public int boo(){return 5;}
     public int foo(){return 0;}
-    public int goo(){return 0;}
+    public int goo(){return 1;}
 
     public int RTerror(){
         return 1;
