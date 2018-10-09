82ea3ef603fcb0a3c2bd1a4fc54c6df4797aef7e
rotba <rotba@post.bgu.ac.il>
2018-10-09 17:42:51 +0300

    delta_3_Test() will pass when patched

--- sub_mod_1/src/main/java/p_1/Amit.java
+++ sub_mod_1/src/main/java/p_1/Amit.java
@@ -4,6 +4,7 @@ public class Amit {
     public int hoo(){return 5;}
     public int boo(){return 5;}
     public int foo(){return 0;}
+    
     public int goo(){return 0;}
 
     public int RTerror(){
