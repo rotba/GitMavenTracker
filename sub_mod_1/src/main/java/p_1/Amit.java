79ae6f6d3be8973bbffa338991b3b1f2f719ebfc
Rotem <rotba@post.bgu.ac.il>
2019-03-02 14:04:39 +0200

    bug in sub_mod_1.p1.Amit - when n is not Even shoouls triple the number

--- sub_mod_1/src/main/java/p_1/Amit.java
+++ sub_mod_1/src/main/java/p_1/Amit.java
@@ -5,8 +5,19 @@ public class Amit {
     public int boo(){return 5;}
     public int foo(){return 0;}
     public int goo(){return 0;}
+	public int doo(int n){
+		int ans;
+		if( isEven(n)
+			ans  = 2 * n;
+		else
+			ans  = 2 * n;
+		return ans;
+	}
 
     public int RTerror(){
         return 1;
+    }
+	public boolean isEven(int n){
+        return n%2 == 0;
     }
 }
